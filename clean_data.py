4d593855ece7162391fc0e5d700ffb4a915a193f
Piotr Cichacki <piotr.cichacki00@gmail.com>
2022-12-17 17:57:34 +0100

    data cleaning

---
+++ clean_data.py
@@ -0,0 +1,55 @@
+import os
+
+import numpy as np
+from sklearn.metrics.pairwise import cosine_distances
+from utils.recognition import extract_person_feature_vectors
+
+
+def search_for_duplicates(matrix, threshold):
+    
+    duplicates_indices = np.argwhere(matrix <= threshold).tolist()
+    duplicates_images_indices = [higher_index for lower_index, higher_index in duplicates_indices if higher_index > lower_index]
+    duplicates_images_indices_unique = list(set(duplicates_images_indices))
+    
+    return duplicates_images_indices_unique
+
+
+def search_for_outliers(matrix, threshold):
+
+    outliers_indices = np.where(np.mean(matrix, axis=1) > threshold)[0].tolist()
+
+    return outliers_indices
+
+
+def remove_images(image_folder_path, indices):
+    for idx, image_file in enumerate(sorted(os.listdir(image_folder_path), key=lambda file_name: int(file_name.split(".")[0]))):
+        image_file_path = os.path.join(image_folder_path, image_file)
+
+        if idx in indices:
+            os.remove(image_file_path)
+
+
+if __name__ == "__main__":
+
+    # similarity_threshold = 0.01
+    # data_folder_path = "data/02_intermediate"
+
+    outliers_threshold = 0.75
+    data_folder_path = "data/03_final"
+
+    for person_folder in os.listdir(data_folder_path):
+        person_folder_path = os.path.join(data_folder_path, person_folder)
+        if os.path.isdir(person_folder_path):
+            print(f"Person: {person_folder.replace('_', ' ').title()}")
+            feature_vectors = extract_person_feature_vectors(person_folder_path)
+            print(f"Feature vectors shape: {feature_vectors.shape}")
+            distances_matrix = cosine_distances(feature_vectors, feature_vectors)
+            print(f"Distance matrix shape: {distances_matrix.shape}")
+            
+            # duplicated_images_indices = search_for_duplicates(distances_matrix, similarity_threshold)
+            # print(f"Number of duplicated images: {len(duplicated_images_indices)}")
+            # remove_images(person_folder_path, duplicated_images_indices)
+
+            outliers_indices = search_for_outliers(distances_matrix, outliers_threshold)
+            print(f"Number of outliers images: {len(outliers_indices)}")
+            remove_images(person_folder_path, outliers_indices)
--- nn_model/model.py
+++ nn_model/model.py
@@ -1,9 +1,9 @@
-# TODO: build model based on the parameters.yml
+# TODO: build model based on the model_1.yml
 def build_model():
     pass
 
 
-# TODO: train model based on the parameters.yml
+# TODO: train model based on the model_1.yml
 def train_model():
     pass
 
