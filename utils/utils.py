1a2bb403692be50bb56ace1bc5aa78baca4283b9
JakGorn <jakgor00@gmail.com>
2022-12-18 14:03:39 +0100

    Hyperparameter tunning

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,10 @@
 import yaml
 
+import io
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
 
 def load_yaml(file_path):
     with open(file_path, "r") as file:
@@ -7,3 +12,34 @@ def load_yaml(file_path):
             return yaml.safe_load(file)
         except yaml.YAMLError as err:
             print(err)
+
+
+def plot_confusion_matrix(cm, class_names):
+    figure = plt.figure(figsize=(16, 16))
+    plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+    plt.title("Confusion matrix")
+    plt.colorbar()
+    tick_marks = np.arange(len(class_names))
+    plt.xticks(tick_marks, class_names, rotation=90)
+    plt.yticks(tick_marks, class_names)
+
+    for i in range(len(cm)):
+        for j in range(len(cm[i])):
+            plt.text(j, i, cm[i][j], horizontalalignment="center")
+
+    plt.tight_layout()
+    plt.ylabel("True label")
+    plt.xlabel("Predicted label")
+    return figure
+
+
+def plot_to_image(figure):
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png")
+    plt.close(figure)
+    buf.seek(0)
+
+    image = tf.image.decode_png(buf.getvalue(), channels=4)
+    image = tf.expand_dims(image, 0)
+
+    return image
